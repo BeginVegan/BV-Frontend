bcd52e2662ecb903d61d15153a0efc8f361c5015
cmoon <chihun0507@naver.com>
2023-05-18 12:00:14 +0900

    feat: :sparkles: favicon 추가 + sweetalert 추가

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -2,16 +2,25 @@ import { isAuthenticatedAtom } from '@/utils/atoms/isAuthenticatedAtom';
 import { Button, Text, VStack } from '@chakra-ui/react';
 import { useAtom } from 'jotai';
 import { Navigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
   const [isAuthenticated, setIsAuthenticated] = useAtom(isAuthenticatedAtom);
 
+  const submitLogin = () => {
+    setIsAuthenticated(true);
+    Swal.fire({
+      icon: 'success',
+      title: '로그인 성공',
+      text: '~님 반가워요',
+    });
+  };
   return (
     <>
       {isAuthenticated ? <Navigate to="/main" /> : null}
       <VStack>
         <Text>Login Page</Text>
-        <Button onClick={() => setIsAuthenticated(true)}>로그인 하기</Button>
+        <Button onClick={submitLogin}>로그인 하기</Button>
       </VStack>
     </>
   );
