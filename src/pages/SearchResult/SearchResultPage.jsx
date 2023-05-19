e291ab88e8ecbf31bf5b2a54ab474f3cecc4e1ce
cmoon <chihun0507@naver.com>
2023-05-19 15:02:44 +0900

    feat: :sparkles: 백엔드 연결예시추가

--- src/pages/SearchResult/SearchResultPage.jsx
+++ src/pages/SearchResult/SearchResultPage.jsx
@@ -6,9 +6,14 @@ import { useParams } from 'react-router-dom';
 const SearchResultPage = () => {
   const { query } = useParams();
   const { status, data, error, isFetching } = useQuery('fetchLuke', async () => {
-    const { data } = await axios.get('http://swapi.dev/api/people/1/');
+    const { data } = await axios.get('http://15.165.34.129:3000/member/list', null, {
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
     return data;
   });
+
   console.log(data);
   return (
     <VStack>
