7f79d977d1a18d68d39c0769c81d9689a569fc71
hasunghwa <tjdghk3920@naver.com>
2023-05-24 14:47:43 +0900

    refactor: :recycle: 누락된 footer import 추가

--- src/components/Layout/DefaultLayout.jsx
+++ src/components/Layout/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Layout/Header';
 import SearchBar from '@/components/search/SearchBar';
 import { Outlet } from 'react-router-dom';
+import Footer from '@/components/Layout/Footer';
 
 const DefaultLayout = ({ hasFooter }) => {
   return (
