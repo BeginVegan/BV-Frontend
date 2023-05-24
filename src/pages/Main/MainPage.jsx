5b95466c18a03e0c9d24e55845a880324acfbbc0
hasunghwa <tjdghk3920@naver.com>
2023-05-24 14:44:08 +0900

    refactor: :recycle: mainPage에서 bestPage연결

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { Box, Container, Flex, Heading, Stack, useBreakpointValue } from '@chakra-ui/react';
 import Hero from '@/components/Layout/Hero';
 import BestStoreCard from '@/components/main/BestStoreCard';
+import { ROUTES } from '@/routes/ROUTES';
 
 const MainPage = () => {
-//   const navigate = useNavigate();
-//   const handleKeyDown = e => {
-//     if (e.key === 'Enter') handleSubmit();
-//   };
-//   const handleSubmit = () => {
-//     if (inputRef === null || inputRef.current.value === '') {
-//       Swal.fire({
-//         icon: 'question',
-//         title: '검색어를 입력해주세요',
-//         text: '지역, 식당 또는 음식',
-//       });
-//       return;
-//     }
-//     const query = inputRef.current.value;
-//     console.log(query);
-//     navigate('/search/' + query);
-//   };
-//   const inputRef = useRef(null);
+  //   const navigate = useNavigate();
+  //   const handleKeyDown = e => {
+  //     if (e.key === 'Enter') handleSubmit();
+  //   };
+  //   const handleSubmit = () => {
+  //     if (inputRef === null || inputRef.current.value === '') {
+  //       Swal.fire({
+  //         icon: 'question',
+  //         title: '검색어를 입력해주세요',
+  //         text: '지역, 식당 또는 음식',
+  //       });
+  //       return;
+  //     }
+  //     const query = inputRef.current.value;
+  //     console.log(query);
+  //     navigate('/search/' + query);
+  //   };
+  //   const inputRef = useRef(null);
   return (
     <>
       <Hero />
@@ -41,19 +42,19 @@ const MainPage = () => {
               heading={'예약 베스트 10곳'}
               description={'핫한 곳 방문하기'}
               img={'/images/main_card1.jpg'}
-              href={'#'}
+              href={ROUTES.BEST_RAW + 'reservation'}
             />
             <BestStoreCard
               heading={'평점 베스트 10곳'}
               description={'믿고가는 가게'}
               img={'/images/main_card2.jpg'}
-              href={'#'}
+              href={ROUTES.BEST_RAW + 'grade'}
             />
             <BestStoreCard
               heading={'리뷰 베스트 10곳'}
               description={'리뷰가 곧 힘이다'}
               img={'/images/main_card3.jpg'}
-              href={'#'}
+              href={ROUTES.BEST_RAW + 'review'}
             />
           </Flex>
         </Container>
