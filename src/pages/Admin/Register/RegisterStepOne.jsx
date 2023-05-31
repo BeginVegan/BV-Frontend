155e277d0d86fce1e0a6a3890ca26cc0f768cbdc
hasunghwa <tjdghk3920@naver.com>
2023-05-31 15:28:32 +0900

    refactor: :recycle: 식당 등록페이지 식당정보와 메뉴정보 분리

--- src/pages/Admin/Register/RegisterStepOne.jsx
+++ src/pages/Admin/Register/RegisterStepOne.jsx
@@ -1,27 +1,28 @@
 import {
-  Box,
   Button,
   HStack,
   Input,
-  NumberInput,
-  NumberInputField,
   Select,
   Text,
+  NumberInput,
+  NumberInputField,
   VStack,
+  Textarea,
 } from '@chakra-ui/react';
 import React from 'react';
+import VeganLevel from '@/components/store/VeganLevel';
 
 const RegisterStepOne = () => {
   return (
     <>
-      <HStack w={'618px'}>
-        <Text w={'90px'} fontWeight={600} color={'gray.600'} fontSize="md">
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
           업체명
         </Text>
         <Input w={'200px'} />
       </HStack>
-      <HStack w={'618px'}>
-        <Text w={'90px'} fontWeight={600} color={'gray.600'} fontSize="md">
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
           전화번호
         </Text>
         <Select w={'100px'}>
@@ -36,8 +37,8 @@ const RegisterStepOne = () => {
           <NumberInputField />
         </NumberInput>
       </HStack>
-      <HStack w={'618px'}>
-        <Text w={'90px'} fontWeight={600} color={'gray.600'} fontSize="md">
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
           주소
         </Text>
         <VStack>
@@ -48,8 +49,8 @@ const RegisterStepOne = () => {
           <Input alignSelf={'flex-start'} w={'400px'} placeholder="상세 주소를 입력해주세요." />
         </VStack>
       </HStack>
-      <HStack w={'618px'}>
-        <Text w={'90px'} fontWeight={600} color={'gray.600'} fontSize="md">
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
           영업시간
         </Text>
         <HStack>
@@ -75,6 +76,56 @@ const RegisterStepOne = () => {
             <option value="30">30분</option>
           </Select>
         </HStack>
+      </HStack>
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
+          식당 상세설명
+        </Text>
+        <VStack alignItems={'flex-start'}>
+          <Text mb="2px">식당 상세 설명(최대 100자)</Text>
+          <Textarea
+            w={'500px'}
+            resize={'none'}
+            placeholder="식당 상세 정보를 입력하세요."
+            size="sm"
+          />
+        </VStack>
+      </HStack>
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
+          1인당 평균 가격
+        </Text>
+        <NumberInput w={'140px'} clampValueOnBlur={false}>
+          <NumberInputField />
+        </NumberInput>
+        <Text fontWeight={600} color={'gray.600'} fontSize="md">
+          원
+        </Text>
+      </HStack>
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
+          예약 가능 인원
+        </Text>
+        <HStack gap={2}>
+          <VStack>
+            <Text mb="2px">테이블 수</Text>
+            <NumberInput w={'140px'} clampValueOnBlur={false}>
+              <NumberInputField />
+            </NumberInput>
+          </VStack>
+          <VStack>
+            <Text mb="2px">테이블 허용 인원</Text>
+            <NumberInput w={'140px'} clampValueOnBlur={false}>
+              <NumberInputField />
+            </NumberInput>
+          </VStack>
+        </HStack>
+      </HStack>
+      <HStack w={'660px'}>
+        <Text w={'132px'} fontWeight={600} color={'gray.600'} fontSize="md">
+          비건레벨
+        </Text>
+        <VeganLevel />
       </HStack>
     </>
   );
