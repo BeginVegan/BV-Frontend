c30a6a31feb8573600ab354d065cbe4a36a0b2e2
cmoon <chihun0507@naver.com>
2023-06-07 14:21:10 +0900

    axios test6

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -4,12 +4,14 @@ const LOCALHOST = 'http://localhost';
 const BACKEND_SERVER = 'https://begin-vegan-backend.kro.kr:3000/';
 
 const headers = {
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  Accept: '*/*',
+  // 기본 헤더 설정 (옵션)
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
 };
 
 const Axios = axios.create({
   baseURL: `${BACKEND_SERVER}/`,
+  timeout: 5000, // 요청 타임아웃 설정 (옵션)
   headers: headers,
   /**
    * js cookie 등을 깔아서 쿠키 설정해줘야함
