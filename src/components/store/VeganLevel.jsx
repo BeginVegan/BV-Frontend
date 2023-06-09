918c005e46d3cf4176693a10dd7cb13256ce1cf6
hasunghwa <tjdghk3920@naver.com>
2023-06-09 09:51:27 +0900

    feat: :sparkles: hook-form으로 VegnaLevel을 컨트롤 하도록 변경

--- src/components/store/VeganLevel.jsx
+++ src/components/store/VeganLevel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconButton, Img, Stack } from '@chakra-ui/react';
 import fishIcon from '@/assets/icons/icons-fish.png';
 import broccoliIcon from '@/assets/icons/icon-broccoli.png';
@@ -7,44 +7,51 @@ import eggIcon from '@/assets/icons/icons-egg.png';
 import meatIcon from '@/assets/icons/icons-meat.png';
 import chickenIcon from '@/assets/icons/icon-thanksgiving.png';
 
-const VeganLevel = ({ veganLevel, setVeganLevel }) => {
+const VeganLevel = ({ setValue }) => {
+  const [veganLevel, setVeganLevel] = useState(1);
+
+  const changeVeganLevel = level => {
+    setVeganLevel(level);
+    setValue('restaurantVeganLevel', level);
+  };
+
   return (
     <Stack direction={'row'} justifyContent={'center'} gap={3}>
       <IconButton
         bg={'white'}
-        opacity={veganLevel === 0 ? '1' : '0.5'}
-        icon={<Img src={broccoliIcon} />}
-        onClick={() => setVeganLevel(0)}
-      />
-      <IconButton
-        bg={'white'}
         opacity={veganLevel === 1 ? '1' : '0.5'}
-        icon={<Img src={milkIcon} />}
-        onClick={() => setVeganLevel(1)}
+        icon={<Img src={broccoliIcon} />}
+        onClick={() => changeVeganLevel(1)}
       />
       <IconButton
         bg={'white'}
         opacity={veganLevel === 2 ? '1' : '0.5'}
-        icon={<Img src={eggIcon} />}
-        onClick={() => setVeganLevel(2)}
+        icon={<Img src={milkIcon} />}
+        onClick={() => changeVeganLevel(2)}
       />
       <IconButton
         bg={'white'}
         opacity={veganLevel === 3 ? '1' : '0.5'}
-        icon={<Img src={fishIcon} />}
-        onClick={() => setVeganLevel(3)}
+        icon={<Img src={eggIcon} />}
+        onClick={() => changeVeganLevel(3)}
       />
       <IconButton
         bg={'white'}
         opacity={veganLevel === 4 ? '1' : '0.5'}
-        icon={<Img src={chickenIcon} />}
-        onClick={() => setVeganLevel(4)}
+        icon={<Img src={fishIcon} />}
+        onClick={() => changeVeganLevel(4)}
       />
       <IconButton
         bg={'white'}
         opacity={veganLevel === 5 ? '1' : '0.5'}
+        icon={<Img src={chickenIcon} />}
+        onClick={() => changeVeganLevel(5)}
+      />
+      <IconButton
+        bg={'white'}
+        opacity={veganLevel === 6 ? '1' : '0.5'}
         icon={<Img src={meatIcon} />}
-        onClick={() => setVeganLevel(5)}
+        onClick={() => changeVeganLevel(6)}
       />
     </Stack>
   );
