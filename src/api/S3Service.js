624c8f851a793fd7150808b653db93783fd24c11
hasunghwa <tjdghk3920@naver.com>
2023-06-19 15:02:52 +0900

    feat: :sparkles: 식당 수정 api 로직 추가

---
+++ src/api/S3Service.js
@@ -0,0 +1,13 @@
+import Axios from '@/api/apiConfig';
+
+const S3Service = {
+  getS3image: async dirName => {
+    const response = await Axios.get('/s3/download', {
+      responseType: 'arraybuffer',
+      params: { dirName: encodeURIComponent(dirName) },
+    });
+    return response.data;
+  },
+};
+
+export default S3Service;
