cc81c406e621f8355ff88bd0c01d29edcc9aff17
hasunghwa <tjdghk3920@naver.com>
2023-06-23 17:11:42 +0900

    refactor: :recycle: Api 서버 배포 서버로 변경

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -11,7 +11,7 @@ const headers = {
 };
 
 const Axios = axios.create({
-  baseURL: `${LOCALHOST}`,
+  baseURL: `${BACKEND_SERVER}`,
   timeout: 5000, // 요청 타임아웃 설정 (옵션)
   headers: headers,
   withCredentials: true,
