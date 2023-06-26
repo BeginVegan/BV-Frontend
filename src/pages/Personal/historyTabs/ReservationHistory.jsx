378435c53268d6482bcb6bda909efd4b62c6744d
cmoon <chihun0507@naver.com>
2023-06-26 17:16:38 +0900

    minor fix

--- src/pages/Personal/historyTabs/ReservationHistory.jsx
+++ src/pages/Personal/historyTabs/ReservationHistory.jsx
@@ -140,7 +140,7 @@ const ReservationHistory = () => {
                     <CustomTd>{store.reservationStatus}</CustomTd>
                     <CustomTd>{formatDate(store.reservationVisitTime)}</CustomTd>
                     <CustomTd>
-                      {Number(isCancellable(store.reservationTime)) < 0 ? (
+                      {Number(isCancellable(store.reservationVisitTime)) < 0 ? (
                         <Button
                           colorScheme="red"
                           size={{ base: 'xs', md: 'sm' }}
